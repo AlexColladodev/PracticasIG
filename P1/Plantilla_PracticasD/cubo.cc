0cd53e9dfda3672ab9471d6325455891a66ac8c3
SatoriAlex <colladoalexander252@gmail.com>
2023-09-29 16:17:44 +0200

    Primer commit, cubo y trigonometria sin colores

---
+++ P1/Plantilla_PracticasD/cubo.cc
@@ -0,0 +1,59 @@
+#include "auxiliar.h"
+#include "malla.h"
+#include "cubo.h"
+
+Cubo::Cubo(float lado)
+{
+
+   this->lado = lado;
+   createVertexArray(this->lado);   //Vertices
+   createTablaTriangulos();         //Tabla de triangulos
+   createColorCubo();
+
+}
+
+//Son 9 vertices
+   //Uno de sus vertices en el origen
+void Cubo::createVertexArray(float lado){
+
+   v.push_back({0, 0, 0});         //0
+   v.push_back({lado, 0, 0});      //1
+   v.push_back({0, lado, 0});      //2
+   v.push_back({lado, lado, 0});   //3
+   v.push_back({lado, 0, -lado});   //4
+   v.push_back({lado, lado, -lado});   //5
+   v.push_back({0, lado, -lado});   //6
+   v.push_back({0, 0, -lado});   //7
+}
+
+void Cubo::createTablaTriangulos(){
+
+   //Cara frontal
+   f.push_back({0, 1, 2});
+   f.push_back({1, 3, 2});
+
+   //Cara derecha
+   f.push_back({1, 4, 3});
+   f.push_back({4, 5, 3});
+
+   //Cara trasera
+   f.push_back({4, 7, 5});
+   f.push_back({7, 6, 5});
+
+   //Cara izquierda
+   f.push_back({7, 0, 6});
+   f.push_back({0, 2, 6});
+
+   //Cara superior
+   f.push_back({3, 5, 2});
+   f.push_back({5, 6, 2});
+
+   //Cara inferior
+   f.push_back({0, 7, 1});
+   f.push_back({7, 4, 1});  
+}
+
+void Cubo::createColorCubo(){
+   c.push_back({0 , 0, 0});
+}
+
---
+++ P1/Plantilla_PracticasD/cubo.h
@@ -0,0 +1,44 @@
+// #############################################################################
+//
+// Informática Gráfica (Grado Informática)
+//
+// Archivo: Cubo.h
+
+//
+// #############################################################################
+
+#ifndef CUBO_H_INCLUDED
+#define CUBO_H_INCLUDED
+
+#include "auxiliar.h"
+
+// *****************************************************************************
+//
+// clases derivadas de Malla3D (definen constructores específicos)
+//
+// *****************************************************************************
+
+// *****************************************************************************
+// Cubo con centro en el origen y lado unidad por defecto
+// (tiene 9 vertices y 6 caras)
+
+class Cubo : public Malla3D
+{
+
+   private:
+      float lado;
+
+   public:
+      Cubo(float l=1) ;
+
+   private:
+      void createVertexArray(float lado);
+      void createTablaTriangulos();
+      void createColorCubo();
+
+} ;
+
+
+
+
+#endif
